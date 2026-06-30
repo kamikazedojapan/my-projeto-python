eefb3a074177cc93105684f91e3c51dd4f0c1c38
Ana Gabriela <gabrielaninha604@gmail.com>
2026-06-30 11:48:25 -0300

    Criação do programa de verificação de idade

---
+++ main.py
@@ -0,0 +1,10 @@
+print ("Sistema de verificação de idade")
+
+nome = input("Digite seu nome")
+
+idade = int(input("Digite sua idade:"))
+
+if idade >= 18:
+    print ("Maior de idade")
+else:
+    print ("Menor de idade")
